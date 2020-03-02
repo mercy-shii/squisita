faf531b0737bec2c15b1c1064acfba9a083c3648
mercy-shii <wemercy525@gmail.com>
2020-03-02 14:50:16 +0300

    squisita

--- js/script.js
+++ js/script.js
@@ -51,36 +51,36 @@ $(document).ready(function(){
                                '</div>');
   });
 
-  $("form#new-contact").submit(function(event) {
+  $("form#new-delivery").submit(function(event) {
     event.preventDefault();
 
-    var inputtedFirstName = $("input#new-first-name").val();
-    var inputtedLastName = $("input#new-last-name").val();
-    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+    var inputtedName = $("input#new-Name").val();
+    var inputtedemail = $("input#new-Email").val();
+    var newdelivery = new delivery(inputtedName, inputtedEmail);
 
     $(".new-address").each(function() {
       var inputtedStreet = $(this).find("input.new-street").val();
       var inputtedCity = $(this).find("input.new-city").val();
       var inputtedCounty = $(this).find("input.new-county").val();
       var newAddress = new Address(inputtedStreet, inputtedCity, inputtedCounty)
-      newContact.addresses.push(newAddress)
+      newproperty.addresses.push(newAddress)
     });
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+    $("ul#contacts").append("<li><span class='contact'>" + newdelivery.Name() + "</span></li>");
 
-    $(".contact").last().click(function() {
+    $(".addres").last().click(function() {
       $("#show-contact").show();
-      $("#show-contact h2").text(newContact.fullName());
-      $(".first-name").text(newContact.firstName);
-      $(".last-name").text(newContact.lastName);
+      $("#show-contact h2").text(newdelivery.Name());
+      $(".Name").text(newContact.firstName);
+      $(".Email").text(newContact.lastName);
       $("ul#addresses").text("");
-      newContact.addresses.forEach(function(address) {
+      newdelivery.addresses.forEach(function(address) {
         $("ul#addresses").append("<li>" + address.street + ", " + address.city + " " + address.county + "</li>");
       });
     });
 
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
+    $("input#new-Name").val("");
+    $("input#new-Email").val("");
     $("input.new-street").val("");
     $("input.new-city").val("");
     $("input.new-county").val("");
