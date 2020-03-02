244fd47f5bce814007e33ade79e826ac8fe18998
mercy-shii <wemercy525@gmail.com>
2020-03-02 12:00:31 +0300

    squisita

--- js/script.js
+++ js/script.js
@@ -1,34 +1,91 @@
+var newdelivery;
 //business logic
 function Delivery(Name,Email){
     this.Name=Name;
     this.Email=Email;
-    this.Adress[];
-
+    //this.Adress[];
 function Delivery(Name,Email){
     this.Name=Name;
     this.Email=Email;
 } 
-contact.prototype.Name = function(){
+Delivery.prototype.Name = function(){
     return this.Name + "" +this.Email;
 }  
 }
 //user interface logic
 $(document).ready(function(){
-    $("form#new-delivery").submit(function(event){
+    $("form#new-Delivery").submit(function(event){
         event.preventDefault();
         var inputtedName = $("input#new-Name").val();
         var inputtedEmail = $("input#new-Email").val();
-        var new-delivery = new delivery(inputtedName,inputtedEmail);
-
-        $("ul#contacts")append("<li><span class='contact'"> +newcontact.Name + '' + newcontact.Email "</span><li>");
+        newdelivery = new Delivery(inputtedName,inputtedEmail);
+        
+        $("ul#contacts").append("<li><span class='contact'>" +newdelivery.Name + '' + newdelivery.Email + "</span><li>");
         $("input#new-Name").val("");
         $("input#new-Email").val("");
-    })
+        
+    
+    
+    });
     $("#contacts").click(function(){
-        ("#show-contact").show();
-        ("#shw-contact h2")text (newcontact.Name);
-        ("#.Name").text(newcontact.Name);
-        ("#.Email").text(newcontact.Email);
-    })
+        $("#show-contact").show();
+        $("#show-contact h2").text(newdelivery.Name);
+        $(".Name").text(newdelivery.Name);
+        $(".Email").text(newdelivery.Email);
+    });
 
-})
+  $("#add-address").click(function() {
+    $("#new-addresses").append('<div class="new-address">' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-street">Street</label>' +
+                                   '<input type="text" class="form-control new-street">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-city">City</label>' +
+                                   '<input type="text" class="form-control new-city">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-county">County</label>' +
+                                   '<input type="text" class="form-control new-county">' +
+                                 '</div>' +
+                               '</div>');
+  });
+
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+    $(".new-address").each(function() {
+      var inputtedStreet = $(this).find("input.new-street").val();
+      var inputtedCity = $(this).find("input.new-city").val();
+      var inputtedCounty = $(this).find("input.new-county").val();
+      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedCounty)
+      newContact.addresses.push(newAddress)
+    });
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.fullName());
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+      $("ul#addresses").text("");
+      newContact.addresses.forEach(function(address) {
+        $("ul#addresses").append("<li>" + address.street + ", " + address.city + " " + address.county + "</li>");
+      });
+    });
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+    $("input.new-street").val("");
+    $("input.new-city").val("");
+    $("input.new-county").val("");
+
+  });
+
+
+});
